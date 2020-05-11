f8b1e5b5e097fb44a256f869afbad120fefbf0a8
nehramosrosa <nehramosrosa@gmail.com>
2020-05-10 18:25:35 -0600

    MVC Conf

---
+++ Sales.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sales.Backend.Models
+{
+    using Domain.Models;
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
+    }
+
+}
